6413198774d3e5c14d8e67fcd464bf4ead59fda2
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2022-09-27 09:28:22 -0500

    Menú Consola dt

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -4,8 +4,38 @@ namespace Torneo.App.Consola
 {
     class Program
     {
-        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        private static IRepositorioDT _repoDT = new RepositorioDT();
         static void Main(string[] args)
-        {}
+        {
+            int opcion = 0;
+            do {
+                Console.WriteLine("2. Insertar Director Técnico");
+                Console.WriteLine("0. Salir");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 2:
+                        AddDT();
+                        break;
+                }
+                }while (opcion !=0);
+        }
+        private static void AddDT()
+        {
+            Console.WriteLine("Escriba el nombre del DT");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escriba el número de Documento");
+            string documento = Console.ReadLine();
+            Console.WriteLine("Escriba el telefono del DT");
+            string telefono = Console.ReadLine();
+            var directorTecnico = new DirectorTecnico           
+            {
+                Nombre = nombre,
+                Documento = documento,
+                Telefono = telefono, 
+            };
+            _repoDT.AddDT(directorTecnico);
+        }
     }
 }
+    
